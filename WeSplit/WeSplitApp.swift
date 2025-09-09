57b828a7565f93f65b4adb890496fabda3c8def4
Marcelo <DEV.marceloakutsu@gmail.com>
2025-09-09 21:24:32 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by AMTK on 2025/09/09.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
